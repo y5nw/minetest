46614a60a44ab65848b2e251272cb715efd2a0f1
y5nw <y5nw@protonmail.com>
2024-06-04 14:38:39 +0200

    minor

--- src/script/lua_api/l_async.cpp
+++ src/script/lua_api/l_async.cpp
@@ -1,6 +1,6 @@
 /*
 Minetest
-Copyright (C) 2024 y5nw <y5nw@outlook.com>
+Copyright (C) 2024 y5nw <y5nw@protonmail.com>
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU Lesser General Public License as published by
