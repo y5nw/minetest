fed7379257989682bc27b04e27279e6d59a7ccd7
y5nw <37980625+y5nw@users.noreply.github.com>
2024-09-12 12:50:22 +0200

    allow binding to Return and Space keys

--- src/gui/guiKeyChangeMenu.h
+++ src/gui/guiKeyChangeMenu.h
@@ -39,7 +39,7 @@ public:
 			const std::string &formspec_prepend = "") :
 		super(joystick, guiroot, -1, &g_menumgr, client, guienv, tsrc,
 				sound_manager, nullptr, nullptr, formspec_prepend),
-		has_client(client != nullptr)
+		has_client(client != nullptr), guienv(guienv)
 		{
 			updateFormSource();
 			setFormspecHandler();
@@ -48,6 +48,12 @@ public:
 	/*
 	 Remove and re-add (or reposition) stuff
 	 */
+	void regenerateGui(v2u32 screensize) {
+		super::regenerateGui(screensize);
+		if (!active_key.empty())
+			guienv->setFocus(this);
+	}
+
 	void acceptInput();
 
 	bool OnEvent(const SEvent &event);
@@ -95,5 +101,6 @@ private:
 	std::unordered_map<std::string, bool> control_options;
 	std::string active_key;
 	bool has_client;
+	gui::IGUIEnvironment *guienv;
 	float scroll_position = 0;
 };
